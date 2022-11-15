eae4ee109c23414c4f0a882fa42cc3bd87e7f783
Felipe Campos <felipecampos50123@gmail.com>
2022-11-15 16:49:12 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
+
+import {useFetch} from './hooks/useFetch'
+
+const url = "http://localhost:3000/products"
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("")
+
+  // -------------------------------------- GET -------------------------------------- 
+  const {data: items, httpConfig, loading, error} = useFetch(url) //Essa função localizada em "/hooks/useFetch" substitui a função "useEffect()" abaixo.
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url)
+  //     const data = await res.json()
+  //     setProducts(data)
+  //   }
+
+  //   fetchData( )
+  // }, []);
+
+  // -------------------------------------- POST -------------------------------------- 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    // Abaixo, na constante 'res', se encontra a configuração do HTTP que foi substituido pela função "httpConfig() encontrada no arquivo /hooks/useFetch"
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(product)
+    // })
+
+    // -------------------------------------- PUT -------------------------------------- 
+    // const addedProduct = await res.json()
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]) 
+
+    httpConfig(product, "POST")
+    setName("") //Limpa o campo do input Name
+    setPrice("") //Limpa o campo do input Price
+  }
+
+  // -------------------------------------- DELETE -------------------------------------- 
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE")
+  }
+
+  // HTML
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de produtos</h1>
+
+      {/* -------------------------------------- Loading -------------------------------------- */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+
+      {/* -------------------------------------- GET -------------------------------------- */}
+      {!error && (
+        <ul>
+        {items && items.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+            <button onClick={() => handleRemove(product.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+      )}
+
+      {/* -------------------------------------- POST -------------------------------------- */}
+      <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome: 
+              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+
+              Preço: 
+              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+
+              {loading && (<input type="submit" value="Aguarde" disabled/>)}
+              {!loading && (<input type="submit" value="Criar"/>)}
+            </label>
+          </form>
+      </div>
     </div>
   );
 }
